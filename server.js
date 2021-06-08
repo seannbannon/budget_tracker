23a6142ab0069c53c67e1afa0acd1fcea7c517d1
Sean Bannon <seannbannon@gmail.com>
2021-06-08 13:52:58 -0500

    edited mongoose connect

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -26,3 +26,7 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+
+
+mongodb+srv://SeanB:<passwordbitch>@budgettracker.wga5h.mongodb.net/budget?retryWrites=true&w=majority
