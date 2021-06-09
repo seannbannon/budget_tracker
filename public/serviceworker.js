ef2851361564dc73e6d3f1d4f39bdb0a4c30ea51
Sean Bannon <seannbannon@gmail.com>
2021-06-09 17:57:04 -0500

    removed comma, fixed syntax

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,14 +1,12 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/style.css",
+    "/styles.css",
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "/db.js",
-    "/index.js",
-
-
+    "/index.js"
   ];
   
   const CACHE_NAME = "static-cache-v2";
