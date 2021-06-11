49b9afbe816151859e01ea817c0457e99fafc386
Sean Bannon <seannbannon@gmail.com>
2021-06-11 11:34:25 -0500

    added ||3000

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget" || 3000, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
